56861829982f410dabc8e3dfbd8624763a883b38
jan.siegl@intellishop.ag <jan@siegl-netz.de>
2016-11-12 00:17:48 +0100

    [PUBLIC] Folder

--- src/SocialGamingBundle/Controller/AdminController.php
+++ src/SocialGamingBundle/Controller/AdminController.php
@@ -9,12 +9,19 @@
 namespace SocialGamingBundle\Controller;
 
 
+use Doctrine\DBAL\Types\TextType;
 use SocialGamingBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class AdminController extends Controller
 {
     public function indexAction(){
+
+        $userForm=$this->createFormBuilder(new User())
+        ->add('username',TextType::TEXT)
+        ->add('email',EmailType::class)
+        ->add('isActive',)
 
         return $this->render('SocialGamingBundle:Admin:Admin.html.twig',array('userForm'=>$userForm->createView()));
 
