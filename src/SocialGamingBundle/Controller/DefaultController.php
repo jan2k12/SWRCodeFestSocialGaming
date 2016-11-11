42783ea670d5a2932889fe0fcabd32259d97326b
jan.siegl@intellishop.ag <jan@siegl-netz.de>
2016-11-11 21:04:43 +0100

    [SocialGamingBundle]

---
+++ src/SocialGamingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SocialGamingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SocialGamingBundle:Default:index.html.twig');
+    }
+}
---
+++ src/SocialGamingBundle/SocialGamingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SocialGamingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SocialGamingBundle extends Bundle
+{
+}
