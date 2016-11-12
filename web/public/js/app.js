6779778498f693daaacec7eed6bb8e3467672315
Frithjof Schaefer <Frithjof Schaefer>
2016-11-12 18:12:37 +0100

    Menu v1 implemented

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -13,10 +13,35 @@ var app = {
            };
 
            window.onhashchange();
+
+            const appTitleBarBottom = $(".app-titlebar").outerHeight();
+            $("#app-menu").css({"top": appTitleBarBottom });
+
+
+            const docSize = {width: $(document).height(), height: $(document).height()- appTitleBarBottom};
+            $("#app-menu-cover-area").css({"width": docSize.width, "height": docSize.height, "top": appTitleBarBottom});
         },
 
         navigateTo: function (relativePath) {
             $("#content").load(relativePath);
+        },
+
+        toggleMenu: function (show) {
+            var menuIconClass = "";
+            if(show === true) {
+                $("#app-menu").show();
+                $("#app-menu-cover-area").show();
+            }else if(show === false){
+                $("#app-menu").hide();
+                $("#app-menu-cover-area").hide();
+            }else{
+                $("#app-menu").toggle();
+                $("#app-menu-cover-area").toggle();
+            }
+            $("#app-menu-icon").removeClass("app-menu-icon");
+            $("#app-menu-icon").removeClass("app-menu-icon-opened");
+
+            $("#app-menu-icon").addClass($("#app-menu").is(":visible") ? "app-menu-icon-opened" : "app-menu-icon");
         }
     };
 
