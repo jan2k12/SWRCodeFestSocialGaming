0daf041d24c2506d98458a7e1648c9519fc9f7db
Frithjof Schaefer <Frithjof Schaefer>
2016-11-12 14:40:18 +0100

    Fix hash navigation

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -3,24 +3,23 @@
  */
 
 var app = {
-    init: function(){
-        window.onhashchange = function(){
-            var currentHash = window.location.hash;
-            if(currentHash.length > 0){
-                currentHash = currentHash.substr(1);
-                app.navigateTo(currentHash);
-            }
-        };
+        init: function () {
+           window.onhashchange = function(){
+               if(window.location.hash.length > 0){
+                   app.navigateTo(window.location.hash.substr(1));
+               }else{
+                   app.navigateTo("/frontend/startFront");
+               }
+           };
 
-        window.onhashchange();
-    },
+           window.onhashchange();
+        },
 
-    navigateTo: function(relativePath){
-        $("#content").load(relativePath);
-    }
+        navigateTo: function (relativePath) {
+            $("#content").load(relativePath);
+        }
+    };
 
-};
-
-$(document).ready(function(){
+$(document).ready(function () {
     app.init();
 });
