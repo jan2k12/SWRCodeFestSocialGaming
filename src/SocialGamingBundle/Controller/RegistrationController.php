b2cb1d088ebc49f3bb3fc11002e69512c7d63c81
jan.siegl@intellishop.ag <jan@siegl-netz.de>
2016-11-16 23:37:35 +0100

    [CHG] Added Registration

---
+++ src/SocialGamingBundle/Controller/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jan
+ * Date: 16.11.16
+ * Time: 22:06
+ */
+
+namespace SocialGamingBundle\Controller;
+
+
+use Monolog\Handler\LogEntriesHandler;
+use Monolog\Logger;
+use SocialGamingBundle\Entity\User;
+use SocialGamingBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationController extends Controller
+{
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form=$this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            try{
+            $password=$this->get('security.password_encoder')->encodePassword($user,$user->getPlainPassword());
+            $user->setPassword($password);
+            $em=$this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
+            }catch(\Exception $ex){
+                $this->get('logger')->error($ex->getMessage());
+                $error='Sorry Something went Wrong with the Registration';
+            }
+            if(!isset($error)){
+                return $this->render('SocialGamingBundle:User:register_success.html.twig');
+            }else{
+                return $this->render('SocialGamingBundle:User:register.html.twig',array('error'=>$error,'form'=>$form->createView()));
+            }
+        }else{
+            $error=$form->getErrors();
+        }
+
+        return $this->render('SocialGamingBundle:User:register.html.twig',array('form'=>$form->createView(),'error'=>$error));
+    }
+}
---
+++ src/SocialGamingBundle/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jan
+ * Date: 16.11.16
+ * Time: 22:01
+ */
+
+namespace SocialGamingBundle\Form;
+
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use SocialGamingBundle\Entity\User;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username',TextType::class)
+            ->add('email',EmailType::class,array(
+
+            ))
+            ->add('plainPassword',RepeatedType::class,array(
+                'type'=>PasswordType::class,
+                'first_options'=>array('label'=>'Password'),
+                'second_options'=>array('label'=>'Repeat Password')
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array('data_class'=>'SocialGamingBundle\Entity\User'));
+    }
+
+
+}
